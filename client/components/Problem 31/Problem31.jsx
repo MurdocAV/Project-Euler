abdc38ea031454e9d5715ee1f7a82f72223b834a
MurdocAV <azaan.virk1@gmail.com>
2019-12-09 13:45:17 +1300

    solution expansion

--- client/components/Problem 31/Problem31.jsx
+++ client/components/Problem 31/Problem31.jsx
@@ -10,6 +10,11 @@ import React from 'react'
 
 class Problem31 extends React.Component { 
 
+  state = {
+    target : 200,
+    denominations : [100, 50, 20, 10, 5, 3, 2, 1]
+  }
+
   displayText (text) {
     return <h1>{text}</h1>
   }
@@ -25,7 +30,17 @@ class Problem31 extends React.Component {
     How to have differnt unique ways.
     Think of soultion before using code...
     1. Combinations? (But 8 different inputs?)
+    2. Try halving method (Simpler conceptually...) //"https://www.maa.org/frank-morgans-math-chat-293-ways-to-make-change-for-a-dollar"
     */
+
+    // Selected methods. Break from largest denomination to the smallest.
+
+    for (let i = 0; i < this.state.denominations.length; i++) {
+      
+      let denomination = this.state.denominations[i] // Given 100p will be used...
+      
+      
+    }
   }
 
   render () {
